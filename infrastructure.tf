bdd0c77a00585461c501595ac68ec7570d005d1b
Aaron Brooks <abrooks@mastermnd.io>
2020-08-05 21:25:36 -0400

    Added ec2 resourse

--- infrastructure.tf
+++ infrastructure.tf
@@ -31,3 +31,15 @@ resource "aws_s3_bucket" "websiteBucket" {
 EOF
   }
 }
+
+# Creates an EC2 instance for development
+resource "aws_instance" "developmentServer" {
+  count         = 2
+  ami           = "ami-0ac80df6eff0e70b5"
+  instance_type = "t3.micro"
+  key_name      = "hoist"
+
+  tags = {
+    Name = "hoist-internal-development"
+  }
+}
