55a7374134940e06c956c6ee39a3f2775ecb8829
Aaron Brooks <abrooks@mastermnd.io>
2020-08-05 21:04:50 -0400

    Added S3 website hosting resource

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,4 +1,33 @@
 provider "aws" {
   version = "~> 3.0"
+  profile = "mastermnd"
   region  = "us-east-1"
+}
+
+# Create an S3 bucket with static hosting enabled
+resource "aws_s3_bucket" "websiteBucket" {
+  bucket = "hoist-media-blog-site"
+  acl    = "public-read"
+  policy = file("policy.json")
+
+  tags = {
+    Name        = "blog bucket"
+    Environment = "Dev"
+  }
+
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
+
+    routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": "documents/"
+    }
+}]
+EOF
+  }
 }
